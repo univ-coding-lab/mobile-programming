66a72341fbcdc3967e080d6f469487cc572af84b
unifolio0 <boho82ha@gmail.com>
2025-04-17 13:42:04 +0900

    feat: 스택 내용 로그

--- cweek07a/src/main/java/com/example/cweek07a/example01/uicomponents/MainScreen.kt
+++ cweek07a/src/main/java/com/example/cweek07a/example01/uicomponents/MainScreen.kt
@@ -1,12 +1,20 @@
 package com.example.cweek07a.example01.uicomponents
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.cweek07a.example01.navGraph.NavGraph
 
+@SuppressLint("RestrictedApi")
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
+    navController.addOnDestinationChangedListener { _, _, _ ->
+        navController.currentBackStack.value.forEachIndexed { index, navBackStackEntry ->
+            Log.d("BackStack", "$index ${navBackStackEntry.destination.route}")
+        }
+    }
     NavGraph(navController = navController)
 }
